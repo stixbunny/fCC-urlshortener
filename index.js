be29bfe3836b4cc802f974569109a7a76bfeba06
stixbunny <milenko.castillo@gmail.com>
2023-03-22 19:54:24 -0300

    Update index.js - get & post function for urls

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require("dns");
+const url = require('url');
+
+// Mongo and Schema setup
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+  url: {type: String, required: true}
+});
+
+const URLs = mongoose.model("URLs", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -21,4 +34,27 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+// Solution
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/shorturl', (req, res) => {
+  let newUrl = req.body.url;
+  dns.lookup(url.parse(newUrl).hostname, (err, address, family) => {
+    if (err) return console.error(err);
+    const urlRecord = new URLs({url: newUrl});
+    urlRecord.save((err, insertedUrl) => {
+      if(err) return console.error(err);
+      res.json({"original_url": insertedUrl.url,"short_url": insertedUrl.id});
+    });
+  });
+});
+
+app.get('/api/shorturl/:url', (req, res) => {
+  let searchUrl = req.params.url;
+  URLs.find({_id: searchUrl}, (err, urlFound) => {
+    if (err) return console.error(err);
+    res.redirect(urlFound.url);
+  });
 });
